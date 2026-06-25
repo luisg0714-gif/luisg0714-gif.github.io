2a8c63094172c2d26c4ed105c1664fc62365a420
luisg0714-gif <luisguerra0714@gmail.com>
2026-06-25 15:47:22 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,8 @@
 console.log("This is loaded from the external JS file");
+var name = "Luis";
+let school = "Palisades";
+var fun_fact = "I like Chocolate Mint Ice Cream";
+
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Fun Fact ${fun_fact}`);
