7142dd97b042a5227de5fc867afabe1efe267cb7
luisg0714-gif <luisguerra0714@gmail.com>
2026-07-09 15:57:21 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -2,7 +2,7 @@
 var coordinates = [15.199999, -86.241905]
 var popupcode = `<center>
 <h3>Hello from Honduras!</h3>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/b1CeG_KeOG8?si=m7hlJbD5-fLPgdiH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<iframe width="200" height="100" src="https://www.youtube.com/embed/b1CeG_KeOG8?si=m7hlJbD5-fLPgdiH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </center>`;
 var map = L.map('map').setView(coordinates, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
